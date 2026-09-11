5c45d13c141d0173fd95f5307ce0b6f2549c6991
majiakas158-prog <majiakas158@gmail.com>
2026-09-11 23:48:53 +0530

    refactor: update routing structure to use React Router and improve navigation components

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,9 +1,6 @@
-import { useHashRoute } from '../hooks/useHashRoute';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { routes } from './routes';
 
 export default function App() {
-  const route = useHashRoute();
-  const Page = routes[route] ?? routes['/'];
-
-  return <Page />;
+  return <HashRouter><Routes>{routes.map(({ path, element }) => <Route key={path} path={path} element={element} />)}<Route path="*" element={<Navigate to="/" replace />} /></Routes></HashRouter>;
 }
--- client/src/app/routes.jsx
+++ client/src/app/routes.jsx
@@ -3,10 +3,10 @@ import { LandingPage } from '../pages/LandingPage';
 import { SignInPage } from '../pages/SignInPage';
 import { SignUpPage } from '../pages/SignUpPage';
 
-export const routes = {
-  '/': LandingPage,
-  '/signin': SignInPage,
-  '/signup': SignUpPage,
-  '/chef-dashboard': () => <DashboardPage type="chef" />,
-  '/restaurant-dashboard': () => <DashboardPage type="restaurant" />,
-};
+export const routes = [
+  { path: '/', element: <LandingPage /> },
+  { path: '/signin', element: <SignInPage /> },
+  { path: '/signup', element: <SignUpPage /> },
+  { path: '/chef-dashboard', element: <DashboardPage type="chef" /> },
+  { path: '/restaurant-dashboard', element: <DashboardPage type="restaurant" /> },
+];
